73f53cda0e2aa4acb009d90c2371f15e99166a7a
HarunnMetin <mtnmusa07@gmail.com>
2025-09-25 01:12:38 +0300

    loop

--- Part-1/index.js
+++ Part-1/index.js
@@ -85,3 +85,13 @@ for (let i = 1; i <= 50; i++) {
     console.log(i);
   }
 }
+
+for (let i = 1; i <= 50; i++) {
+  if (i % 3 == 0 && i % 5 == 0) {
+    console.log("FizzBuzz");
+  } else if (i % 3 == 0) {
+    console.log("Fizz");
+  } else if (i % 5 == 0) {
+    console.log("Buzz");
+  }
+}
