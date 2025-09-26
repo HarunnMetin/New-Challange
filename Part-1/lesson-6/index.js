7496c1e5d5cdad71e2c1f6d9fd1b8f861529239d
HarunnMetin <mtnmusa07@gmail.com>
2025-09-26 18:58:04 +0300

    object

---
+++ Part-1/lesson-6/index.js
@@ -0,0 +1,20 @@
+
+
+let object = {
+
+    name: "Sevda",
+    job: "Lawyer",
+    age: 43,
+    country: "USA",
+    city: "Pentagon"
+}
+
+console.log(Object.keys(object));
+console.log(Object.values(object));
+console.log(Object.entries(object));
+
+
+
+
+
+
