77d0a425ee8891312acba989655b5e63e4bc2d87
HarunnMetin <mtnmusa07@gmail.com>
2025-09-26 19:15:48 +0300

    object

---
+++ Part-1/lesson-7/index.js
@@ -0,0 +1,28 @@
+/*
+Bir dizi oluştur:
+
+
+
+Dizideki 18 yaşından büyük kişileri filtrele ve yeni bir dizi döndür.
+*/
+
+let kisiler = [
+  { isim: "Ali", yas: 25 },
+  { isim: "Ayşe", yas: 30 },
+  { isim: "Mehmet", yas: 17 },
+  { isim: "Elif", yas: 20 },
+];
+
+let newPersons = [];
+
+let buyuk = kisiler.filter(kisi => kisi.yas > 18);
+newPersons = buyuk;
+
+console.log(newPersons);
+
+
+
+
+
+
+
